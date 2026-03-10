e1ab0db54f5c9de26353009aa73a48005818a83d
prasanna <prasa@LAPTOP>
2026-03-10 17:02:42 +0530

    chore: project structure, express server and mongodb connection setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import cors from 'cors';
+
+// Load env variables
+dotenv.config();
+
+//Connect to mongoDB
+connectDB();
+
+//Middleware
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.json({message: '🍳 SnapChef API is running!'});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
